54d91f4f0eeb974631bd6a2202408d695b9e628d
Venkatanarasimha Reddy Burri <venkat@s-MacBook-Pro.lan>
2023-11-09 11:03:58 -0500

    index.js setup

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const { readdirSync } = require("fs");
+
+const app = express();
+
+mongoose.connect("mongodb+srv://bojjaananya20:<zNfpkOrEkx8ep8qx>@cluster0.e5axad9.mongodb.net/", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log("DB Connected"))
+    .catch((error) => console.log(error));
+
+app.use(morgan("dev"));
+app.use(bodyParser.json({ limit: "2mb" }));
+app.use(cors());
+
+readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
+
+const PORT = 8001;
+
+app.listen(PORT, () => console.log(`Server is running at PORT: ${PORT}`));
